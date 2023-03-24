4140b60b9e51c6db317d4d6d761b383028f6f6bb
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-24 12:24:31 -0500

    before lunch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,29 @@
 public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        System.out.println("How is it going?");
+
+        int myfavnumber = 21;
+        System.out.println("My favorite number is " + myfavnumber);
+
+        String weird = "What's cooking, good looking?";
+
+        System.out.println(weird);
+
+//        weird = 3.141589;
+        long myNumber;
+
+        myNumber = 123;
+
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int y = 5;
+        System.out.println("y is: " + ++y);
+        System.out.println("Y is now: " + y);
+
+//        var class = "something";
+
+    }
 }
