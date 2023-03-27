07615e0c01e57305a6f4e10442112e8b237b1361
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-27 09:59:36 -0500

    before table of powers

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,45 @@
+public class ControlFlowExercises {
+    public static void main (String [] args){
+        System.out.println("Test");
+
+//        int i = 5;
+//
+//        while (i <= 15){
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+//
+//        int x = 100;
+//
+//        do {
+//            x = x - 5;
+//            System.out.println("X is " + x);
+//        }while (x > -10);
+
+//        int y = 2;
+//
+//        do {
+//            y = y * 2;
+//            System.out.println("Y is " + y);
+//        } while (y < 1000000);
+
+//        for(int z = 1; z < 100; z++){
+//            if (z % 2 == 1){
+//                continue;
+//            }
+//            System.out.println("Z is " + z);
+//        }
+
+//        fizz buzz
+        for(int i = 1; i < 100; i++){
+            if(i % 3 == 0){
+                System.out.println(i + " fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println(i + " buzz");
+            }
+//            System.out.println(i);
+        }
+    }
+}
+
