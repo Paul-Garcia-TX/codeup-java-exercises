db11f4e1f664e3bc1596b450de0bcb9e7b64bfff
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-05 22:53:00 -0500

    student test done initially

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    private ArrayList<Double> grades;
+    ArrayList<Double> grades;
 
     public Student (String name){
         this.name = name;
--- src/grades/StudentsTest.java
+++ src/grades/StudentsTest.java
@@ -1,4 +1,27 @@
 package grades;
 
 public class StudentsTest {
+
+    public static void main (String[] args){
+        Student student = new Student("Paul");
+        student.setGrades(84.0);
+        student.setGrades(73.2);
+        student.setGrades(98.3);
+        student.setGrades(88.9);
+
+        Student jack = new Student("Jack");
+
+        jack.setGrades(84.3);
+        jack.setGrades(76.3);
+        jack.setGrades(85.3);
+        jack.setGrades(95.6);
+
+        double jackAvg = jack.getGradeAverage();
+
+        double avg = student.getGradeAverage();
+        System.out.println("Student " + student.getName() + " has the following grades" + student.grades + " which gives him an average of: " + avg);
+
+        System.out.println("Student " + jack.getName() + " has the following grades " + jack.grades + " which gives him an a average of: " + jackAvg);
+    }
+
 }
