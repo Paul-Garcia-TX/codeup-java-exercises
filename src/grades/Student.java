839e99cf3e46f61b6e969506834cd9b05ff91c4e
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-05 22:42:18 -0500

    student builder created

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,29 @@
 package grades;
 
+
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private ArrayList<Double> grades;
+
+    public Student (String name){
+        this.name = name;
+        this.grades = new ArrayList<Double>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setGrades(double grade){
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0.0;
+        for(double grade: grades){
+            sum += grade;
+        }
+        return sum/grades.size();
+    }
 }
