adde5c3f310ffb5edf5d077cba298cc80105a8e0
Paul Garcia <paul.ngarcia@yahoo.com>
2023-04-06 10:00:35 -0500

    still working

---
+++ src/grades/GradesApp.java
@@ -0,0 +1,10 @@
+package grades;
+
+import java.util.HashMap;
+
+public class GradesApp {
+
+    public static void main (String[] args){
+        HashMap <String, double> students = new HashMap<>;
+    }
+}
